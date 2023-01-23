0d7552b7ede4f042a6325b4101288706714b00eb
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-23 13:37:28 -0500

    initialized msql and express

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,44 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-
 SHOW DATABASES;
 
-CREATE TABLE department (
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-);
+CREATE TABLE
+    department (
+        id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL
+    );
 
 -- CREATE TABLE role (
+
 --  id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
 --  title VARCHAR(30) NOT NULL,
+
 --  salary INT,
+
 --  FOREIGN KEY (role_id)
+
 --  REFERENCES department(id)
+
 -- );
 
 --  CREATE TABLE employee (
+
 --   id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
 --   first_name VARCHAR(30) NOT NULL,
+
 --   last_name VARCHAR(30) NOT NULL,
+
 --   FOREIGN KEY (role_id)
+
 --   REFERENCES department(id)
+
 --   FOREIGN KEY (manager_id)
+
 --   REFERENCES role(id)
 
 --  );
 
- SELECT DATABASE();
+SELECT DATABASE();
