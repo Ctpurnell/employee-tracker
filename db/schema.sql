9497a686970f89368358bffd68b473d8edecf7c2
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-23 11:12:59 -0500

    added intial schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+ id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(30) NOT NULL,
+ salary INT,
+ FOREIGN KEY (role_id)
+ REFERENCES department(id)
+);
+
+ CREATE TABLE employee (
+  id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES department(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES role(id)
+
+ );
