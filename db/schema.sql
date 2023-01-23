83351675a93113af5096470421db8b0940a57adb
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-23 16:29:16 -0500

    schema stuff

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,29 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE
+    employee (
+        id INT NOT NULL AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT NOT NULL,
+        manager_id INT,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
     department (
-        id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(30) NOT NULL,
+        PRIMARY KEY (id)
     );
 
 CREATE TABLE
     role (
-        id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        id INT NOT NULL AUTO_INCREMENT,
         title VARCHAR(30) NOT NULL,
-        salary DECIMAL,
-        FOREIGN KEY (role_id) REFERENCES department(id)
-    );
-
-CREATE TABLE
-    employee (
-        id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(30) NOT NULL,
-        last_name VARCHAR(30) NOT NULL,
-        FOREIGN KEY (role_id) REFERENCES department(id) FOREIGN KEY (manager_id) REFERENCES role(id)
+        salary DECIMAL NOT NULL,
+        department_id INT,
+        PRIMARY KEY (id)
     );
 
 SHOW TABLES();
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (id, name)
+VALUES(0001, "Sales"),
+      (0002, "Engineering"),
+      (0003, "Finance"), 
+      (0004, "Legal"),
+      (0005, "Service"),
+      (0006, "Artist");
+
+      
+
+
+
