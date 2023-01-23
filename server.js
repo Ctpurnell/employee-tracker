4e59280fbbd4358fa102c0c4784e413a31213451
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-23 18:18:10 -0500

    iquirer and express

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Ogctp0906@",
   database: "employees_db",
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  listPrompts();
+});
+
+function listPrompts() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to  do?",
+      name: "name",
+    },
+  ]);
+}
