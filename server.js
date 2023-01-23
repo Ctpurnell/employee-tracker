ec37f7ea827aad49dac8250ebdc9d9b46f1391cb
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-01-23 11:44:27 -0500

    started on server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require('mysql2');
+
+const connection = mysql.createConnection( {
+    host: 'localhost',
+    port: 3001
+    user: 'root',
+    password: '',
+    database: 'employees_db'
+});
+
+
